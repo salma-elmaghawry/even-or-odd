f42547affe0dfff8dfbd79b13edda14c3d6e5ea2
salma Elmaghawry <112850475+salma-elmaghawry@users.noreply.github.com>
2023-02-24 22:58:08 +0200

    Add files via upload

---
+++ even and odd with C++/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int Number;
+	cout << "please enter the number\n ";
+		cin >> Number;
+		if (Number % 2 == 0)
+			cout << "This is even number \n";
+		else 
+			cout << "This is odd number \n";
+
+		return 0;
+	
+}
